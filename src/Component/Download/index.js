fed54fabae135edff722358ace0d8cc233aa28aa
Empire726 <naveenraoyadav758@gmail.com>
2021-10-07 14:24:25 +0530

    apkfiless

--- src/Component/Download/index.js
+++ src/Component/Download/index.js
@@ -14,7 +14,7 @@ const Logo = () => {
         <div className="diver">
                 <img className="logo1-image" src={css} alt="none"/>
                 <div className="anchor">
-                    <a href="https://firebasestorage.googleapis.com/v0/b/whitediamond-fb513.appspot.com/o/WhiteDiamond.apk?alt=media&token=0c4b3547-9246-4765-b6fc-1540e1c1e88e" className="btn">DOWNLOAD APK</a>
+                        <a href="https://firebasestorage.googleapis.com/v0/b/whitediamond-fb513.appspot.com/o/WhiteDiamond.apk?alt=media&token=57cdb805-ae78-47f8-afb1-e2c6663e64b4" className="btn">DOWNLOAD APK</a>
                 </div>
                 </div>
         </div>
